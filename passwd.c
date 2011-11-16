4db95a3f32b00bae19006f1a6df5ceabef84039e
Costas Drogos <costasd@noc.grnet.gr>
2011-11-16 15:06:15 +0200

    Config file support:
    New Files:
    * libnss_uidpool.bnf: BNF-like grammar of the config
    * libnss_uidpool.conf: Config file, location determined by #define CONFIG

--- passwd.c
+++ passwd.c
@@ -27,12 +27,93 @@
 
 enum nss_status _nss_uidpool_getpwuid_r(uid_t, struct passwd *, char *, size_t, int *);
 enum nss_status _nss_uidpool_getpwnam_r(const char *, struct passwd *, char *, size_t, int *);
+char * strstrip(char * string, char * trimchars);
+int  fileparser(void);
 
-#define USERNAME_PREFIX "pool"
-#define DEFAULT_GID 65534 /* nogroup */
-#define UID_MIN 10000
-#define UID_MAX 11000
+#define CONFIG_FILE "/etc/libnss_uidpool.conf"
 
+
+/* Some safe defaults first in case we don't have a config file */
+char *USERNAME_PREFIX = "pool";
+char *SHELL = "/bin/false";
+char *HOME  = "/noexistent";
+char *GECOS = "uidpool user,,,";
+int UID_MIN = 10001;
+int UID_MAX = 10101;
+int DEFAULT_GID = 65534;
+
+char * strstrip(char * s, char * t)
+{
+/*
+    This function returns a pointer to a substring
+    of the s, with all occurences of t removed.
+*/
+    char * cleaned;
+    int counter = 0;
+
+    /* at most strlen(s) */
+    cleaned = malloc(strlen(s));
+    while(*s)
+    {
+        if ( !strchr(t, *s) )
+        {
+            cleaned[counter] = *s;
+            counter++ ;
+        }
+        ++s;
+    }
+    return cleaned;
+}
+
+int fileparser(void)
+{
+    FILE *fp;
+    size_t len = 0;
+    ssize_t read;
+    char *key, *value, *saveptr, *clean, *cleaned;
+    char *line = NULL;
+    char *cleaners = "'\"";
+
+    fp = fopen(CONFIG_FILE, "r");
+    if (fp != NULL )
+    {
+        while ((read = getline(&line, &len, fp)) != -1)
+        {
+            /* comments get ignored first */
+            if (line[0] == '#')
+                continue;
+            /* Then we clean usual invalid characters */
+            cleaned = strstrip(line, cleaners);
+            /* a = b, not a == b or a = b = c */
+            if (index(cleaned, '=') == rindex(cleaned, '='))
+            {
+                /* clean newlines first */
+                clean = strsep(&cleaned, "\n");
+                /* Break the key-value pair */
+                key   = strtok_r(clean, " =", &saveptr);
+                value = strtok_r(NULL, " =", &saveptr);
+                /* Our little grammar */
+                if (strcmp(key, "UID_PREFIX") == 0)
+                    USERNAME_PREFIX = value;
+                if (strcmp(key, "UID_MIN") == 0)
+                    UID_MIN = atoi(value);
+                if (strcmp(key, "UID_MAX") == 0)
+                    UID_MAX = atoi(value);
+                if (strcmp(key, "DEFAULT_GID") == 0)
+                    DEFAULT_GID = atoi(value);
+                if (strcmp(key, "SHELL") == 0)
+                    SHELL = value;
+                if (strcmp(key, "HOME") == 0)
+                    HOME  = value;
+                if (strcmp(key, "GECOS") == 0)
+                    GECOS  = value;
+                /* None of our keywords, next line please*/
+            }
+        }
+        fclose(fp);
+    }
+    return 0;
+}
 /* append a string to buf, move buf forward and reduce buflen */
 static int append_to_buf(char **buf, size_t *buflen, const char *string)
 {
@@ -57,12 +138,14 @@ static int append_to_buf(char **buf, size_t *buflen, const char *string)
 
 enum nss_status _nss_uidpool_getpwuid_r(uid_t uid, struct passwd *result, char *buf, size_t buflen, int *errnop)
 {
+    fileparser();
 	*errnop = 0;
 
 	/* the last check is a paranoid safeguard against returning root */
 	if (uid < UID_MIN+1 || uid >= UID_MAX || uid == 0) {
 		return NSS_STATUS_NOTFOUND;
 	}
+
 
 	if (!result || !buf) {
 		return NSS_STATUS_UNAVAIL;
@@ -71,6 +154,7 @@ enum nss_status _nss_uidpool_getpwuid_r(uid_t uid, struct passwd *result, char *
 	result->pw_uid = uid;
 	result->pw_gid = DEFAULT_GID;
 
+
 	#define ASSIGN_FROM_BUF(x, str) { 				\
 		x = buf;				\
 		if (append_to_buf(&buf, &buflen, str) == 1) {	\
@@ -78,11 +162,12 @@ enum nss_status _nss_uidpool_getpwuid_r(uid_t uid, struct passwd *result, char *
 			return NSS_STATUS_TRYAGAIN;		\
 		}						\
 	}
-	
+
 	ASSIGN_FROM_BUF(result->pw_passwd, "!");
-	ASSIGN_FROM_BUF(result->pw_gecos, "uidpool user,,,");
-	ASSIGN_FROM_BUF(result->pw_dir, "/nonexistent");
-	ASSIGN_FROM_BUF(result->pw_shell, "/bin/false");
+	ASSIGN_FROM_BUF(result->pw_gecos, GECOS);
+	ASSIGN_FROM_BUF(result->pw_dir, HOME);
+	ASSIGN_FROM_BUF(result->pw_shell, SHELL);
+
 
 	/* assume that uid length is 10 (i.e. uid_t is 32-bits) */
 	if (buflen < strlen(USERNAME_PREFIX) + 10 + 1) {
@@ -97,6 +182,8 @@ enum nss_status _nss_uidpool_getpwuid_r(uid_t uid, struct passwd *result, char *
 
 enum nss_status _nss_uidpool_getpwnam_r(const char *name, struct passwd *result, char *buf, size_t buflen, int *errnop)
 {
+    fileparser();
+
 	*errnop = 0;
 	if (strncmp(name, USERNAME_PREFIX, strlen(USERNAME_PREFIX)) != 0) {
 		return NSS_STATUS_NOTFOUND;
